f8d49d8ba860ab8291d35f40a61f96214070c7e2
Jonas Birmé <jonas.birme@eyevinn.se>
2024-10-30 00:21:35 +0100

    fix: provide direct db url

--- scripts/docker-start.sh
+++ scripts/docker-start.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
 
-DIRECT_DATABASE_URL=$DATABASE_URL
-prisma migrate deploy --schema=./prisma/schema.prisma
+DIRECT_DATABASE_URL=$DATABASE_URL prisma migrate deploy --schema=./prisma/schema.prisma
 NEXT_PUBLIC_BASE_URL=https://$OSC_HOSTNAME
 NEXTAUTH_URL=$NEXT_PUBLIC_BASE_URL node apps/web/server.js
