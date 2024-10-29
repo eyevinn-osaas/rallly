ca4e4c54b907a248a6d8bdf72992476094d9b11f
Jonas Birmé <jonas.birme@eyevinn.se>
2024-10-30 00:00:35 +0100

    chore: prepare for when OSC supports downward api

--- scripts/docker-start.sh
+++ scripts/docker-start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 set -e
+
+DIRECT_DATABASE_URL=$DATABASE_URL
 prisma migrate deploy --schema=./prisma/schema.prisma
+NEXT_PUBLIC_BASE_URL=https://$OSC_HOSTNAME
 NEXTAUTH_URL=$NEXT_PUBLIC_BASE_URL node apps/web/server.js
