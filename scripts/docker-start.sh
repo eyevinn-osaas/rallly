80c5ed9fc767baef0cbadff1841092c476adc1ad
Jonas Birmé <jonas.birme@eyevinn.se>
2024-10-30 01:08:45 +0100

    fix: auto-generate secret pwd

--- scripts/docker-start.sh
+++ scripts/docker-start.sh
@@ -3,6 +3,7 @@ set -e
 
 echo $OSC_HOSTNAME
 
+SECRET_PASSWORD=`cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1`
 DIRECT_DATABASE_URL=$DATABASE_URL prisma migrate deploy --schema=./prisma/schema.prisma
 NEXT_PUBLIC_BASE_URL="https://$OSC_HOSTNAME"
 NEXTAUTH_URL=$NEXT_PUBLIC_BASE_URL node apps/web/server.js
